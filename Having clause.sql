8f8cf89508a71633fd5fecee272b291d6e5b21f6
MRehman2005 <113408534+MRehman2005@users.noreply.github.com>
2025-04-22 01:48:35 +0500

    Add files via upload

---
+++ Having clause.sql
@@ -0,0 +1,7 @@
+select 
+	 client_id,
+     sum(invoice_total) as total_sales,
+     count(*) as number_of_invoices
+from invoices
+group by client_id
+having total_sales > 500 and number_of_invoices > 5
---
+++ ROLLUp Operator.sql
@@ -0,0 +1,7 @@
+Select 
+	 state,
+     city,
+     sum(invoice_total) as total_sales
+from invoices i
+join clients c using (client_id)
+group by state,city with rollup
